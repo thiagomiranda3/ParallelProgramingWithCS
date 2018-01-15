bac5d9b40a831a5e23c9a93ccff4db5065309c98
Thiago Miranda <marine.rlivielor@gmail.com>
2018-01-15 21:05:26 -0200

    Basic Task

---
+++ BasicTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ParallelProgramingWithCS
+{
+    public class BasicTask
+    {
+        public static void Write(string c)
+        {
+            int x = 0;
+
+            while(x++ < 500)
+            {
+                Console.Write(c);
+                x++;
+            }
+        }
+
+        public static void Main()
+        {
+            Task.Factory.StartNew(() => Write("x"));
+
+            var t = new Task(() => Write("Y"));
+            t.Start();
+
+            Write("-");
+
+            Console.ReadKey();
+        }
+    }
+}
